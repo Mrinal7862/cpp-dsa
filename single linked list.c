67e9aacf1b4231cce26a300ffe2126a211d2d513
Mrinal devnath <codemaxter473@gmail.com>
2025-10-30 00:42:01 +0530

    Update single linked list.c

--- single linked list.c
+++ single linked list.c
@@ -1,129 +1,160 @@
-// C program for all the operations in the
-// Singly Linked List
-
+// // C Program for Implementation of Singly Linked List
 #include <stdio.h>
 #include <stdlib.h>
 
-// Linked List Node
-
-struct node
-{
-    int info;
-    struct node *link;
+// Define the Node structure
+struct Node {
+    int data;
+    struct Node* next;
 };
 
-struct node *start = NULL;
-
-//Function to create list with the n nodes initially 
-
-void createList(){
-    if(start == NULL){
-        int n; 
-        printf("\nEnter the number of nodes: ");
-        scanf("%d", &n);
-
-        if(n!=0){
-            int data; 
-
-            struct node* newnode;
-            struct node* temp;
-            newnode = malloc(sizeof(struct node));
-
-            newnode->link = NULL; 
-
-            start = newnode; 
-
-            temp = start;
-
-            printf("\nEnter number to be interested: ");
-            scanf("%d", &data);
-
-            start->info = data; 
-
-            for(int i = 2; i<=n; i++){
-                newnode = malloc(sizeof(struct node));
-
-                newnode->link = NULL; 
-                temp->link = newnode; 
-
-                printf("\nEnter number to be inserted: "); 
-                scanf("%d", &data);
-
-                newnode->info = data; 
-
-                temp = temp->link;
-            }
-        }
-        printf("\nThe lis is created\n");
-    }
-    else{
-        printf("\nThe list is already created\n"); 
-    }
+// Function to create a new node 
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
 }
 
-// Function to traverse the linked list
-void traverse(){
-    struct node* temp; 
+// Function to insert a new element at the beginning of the singly linked list
+void insertAtFirst(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    newNode->next = *head;
+    *head = newNode;
+}
+
+// Function to insert a new element at the end of the singly linked list
+void insertAtEnd(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    struct Node* temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+// Function to insert a new element at a specific position in the singly linked list
+void insertAtPosition(struct Node** head, int data, int position) {
+    struct Node* newNode = createNode(data);
+    if (position == 0) {
+        insertAtFirst(head,data);
+        return;
+    }
+    struct Node* temp = *head;
+    for (int i = 0; temp != NULL && i < position - 1; i++) {
+        temp = temp->next;
+    }
+    if (temp == NULL) {
+        printf("Position out of range\n");
+        free(newNode);
+        return;
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
+
+// Function to delete the first node of the singly linked list
+void deleteFromFirst(struct Node** head) {
+    if (*head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+    struct Node* temp = *head;
+    *head = temp->next;
+    free(temp);
+}
+
+// Function to delete the last node of the singly linked list
+void deleteFromEnd(struct Node** head) {
+    if (*head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+    struct Node* temp = *head;
+    if (temp->next == NULL) {
+        free(temp);
+        *head = NULL;
+        return;
+    }
+    while (temp->next->next != NULL) {
+        temp = temp->next;
+    }
+    free(temp->next);
+    temp->next = NULL;
+}
+
+// Function to delete a node at a specific position in the singly linked list
+void deleteAtPosition(struct Node** head, int position) {
+    if (*head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+    struct Node* temp = *head;
+    if (position == 0) {
+        deleteFromFirst(head);
+        return;
+    }
+    for (int i = 0; temp != NULL && i < position - 1; i++) {
+        temp = temp->next;
+    }
+    if (temp == NULL || temp->next == NULL) {
+        printf("Position out of range\n");
+        return;
+    }
+    struct Node* next = temp->next->next;
+    free(temp->next);
+    temp->next = next;
+}
+
+// Function to print the LinkedList
+void print(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+// Driver Code
+int main() {
+    struct Node* head = NULL;
     
-    // list is empty
-    if(start == NULL){
-        printf("\nList is empty\n");
-    }
-    // Else print the Linked List
-    else{
-        temp = start; 
-        while (temp!=NULL){
-            printf("Data = %d\n", temp->info);
-            temp = temp->link; 
-        }
-        
-    }
-}
-
-// Funciton to insert  at the front 
-
-void insertAtFront(){
-    int data; 
-    struct node* temp; 
-    temp = malloc(sizeof(struct node)); 
-    printf("\nEnter number to be inserted: "); 
-    scanf("%d", &data); 
-    temp->info = data; 
-
-    // Pointer of temp will be assigned to start 
-
-    temp->link = start; 
-    start = temp; 
-}
-
-// Function to insert at the end of the linked list 
-
-void insertAtEnd(){
-    int data; 
-    struct node *temp, *head; 
-    temp = malloc(sizeof(struct node));
-
-    // Enter the number 
-    printf("\nEnter number to be inserted: "); 
-    scanf("%d", &data);
-
-    // changeslinks
-    temp->link = 0; 
-    temp->info = data; 
-    head = start; 
+    insertAtFirst(&head, 10);
+    printf("Linked list after inserting the node:10 at the beginning \n");
+    print(head); 
     
-    while(head->link !=NULL){
-        head = head->link;
-
-    }
-    head->link = temp;
-}
-
-// Function to insert at any specified position the linked list
-
-
-int main()
-{
+    printf("Linked list after inserting the node:20 at the end \n");
+    insertAtEnd(&head, 20);
+    print(head); 
+    
+    printf("Linked list after inserting the node:5 at the end \n");
+    insertAtEnd(&head, 5);
+    print(head); 
+    
+    printf("Linked list after inserting the node:30 at the end \n");
+    insertAtEnd(&head, 30);
+    print(head); 
+    
+    printf("Linked list after inserting the node:15 at position 2 \n");
+    insertAtPosition(&head, 15, 2);
+    print(head);
+    
+    printf("Linked list after deleting the first node: \n");
+    deleteFromFirst(&head);
+    print(head); 
+    
+    printf("Linked list after deleting the last node: \n");
+    deleteFromEnd(&head);
+    print(head); 
+    
+    printf("Linked list after deleting the node at position 1: \n");
+    deleteAtPosition(&head, 1);
+    print(head); 
 
     return 0;
 }
